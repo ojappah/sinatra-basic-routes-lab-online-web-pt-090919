ca3e7a9f59bb07a351bb8be0b9f193fbf76c9865
generous-vector-9581 <ojappah@gmail.com>
2020-01-01 18:29:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is OJ"
+  end
+
+  get '/hometown' do
+    "My hometown is Monrovia"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'When it was me'"
+  end
 end
